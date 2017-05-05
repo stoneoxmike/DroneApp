a3aa054822d28f201fed289df5103fb7644395a4
stoneoxmike <stoneoxmike@gmail.com>
2017-05-05 14:26:19 -0400

    5/5/17 updated

--- app/src/main/java/org/pltw/examples/droneapp/DroneActivity.java
+++ app/src/main/java/org/pltw/examples/droneapp/DroneActivity.java
@@ -2,10 +2,14 @@ package org.pltw.examples.droneapp;
 
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,17 +21,24 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parrot.arsdk.ARSDK;
+import com.parrot.arsdk.arcommands.ARCOMMANDS_ARDRONE3_MEDIARECORDEVENT_PICTUREEVENTCHANGED_ERROR_ENUM;
+import com.parrot.arsdk.arcommands.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM;
+import com.parrot.arsdk.arcontroller.ARCONTROLLER_DEVICE_STATE_ENUM;
+import com.parrot.arsdk.arcontroller.ARControllerCodec;
+import com.parrot.arsdk.arcontroller.ARFrame;
 import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceService;
+
+import static com.parrot.arsdk.arcommands.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_HOVERING;
 
 
 public class DroneActivity extends FragmentActivity implements OnMapReadyCallback {
-    private static final String TAG = "BebopActivity";
+    private static final String TAG = "DroneActivity";
     private Drone mDrone;
 
     private ProgressDialog mConnectionProgressDialog;
     private ProgressDialog mDownloadProgressDialog;
 
-    //private BebopVideoView mVideoView;
+    private DroneVideoView mVideoView;
 
     private TextView mBatteryLabel;
     //private Button mTakeOffLandBt;
@@ -54,7 +65,6 @@ public class DroneActivity extends FragmentActivity implements OnMapReadyCallbac
         ARDiscoveryDeviceService service = intent.getParcelableExtra(DeviceListActivity.EXTRA_DEVICE_SERVICE);
         mDrone = new Drone(this, service);
         mDrone.addListener(mDroneListener);
-        ARSDK.loadSDKLibs();
         Location currentLocation = new Location("Project Fi");
         Lat = currentLocation.getLatitude();
         Lng = currentLocation.getLongitude();
@@ -82,4 +92,139 @@ public class DroneActivity extends FragmentActivity implements OnMapReadyCallbac
         mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocationMarker));
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // show a loading view while the bebop drone is connecting
+        if ((mDrone != null) && !(ARCONTROLLER_DEVICE_STATE_ENUM.ARCONTROLLER_DEVICE_STATE_RUNNING.equals(mDrone.getConnectionState())))
+        {
+            mConnectionProgressDialog = new ProgressDialog(this, R.style.AppCompatAlertDialogStyle);
+            mConnectionProgressDialog.setIndeterminate(true);
+            mConnectionProgressDialog.setMessage("Connecting ...");
+            mConnectionProgressDialog.setCancelable(false);
+            mConnectionProgressDialog.show();
+
+            // if the connection to the Bebop fails, finish the activity
+            if (!mDrone.connect()) {
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrone != null)
+        {
+            mConnectionProgressDialog = new ProgressDialog(this, R.style.AppCompatAlertDialogStyle);
+            mConnectionProgressDialog.setIndeterminate(true);
+            mConnectionProgressDialog.setMessage("Disconnecting ...");
+            mConnectionProgressDialog.setCancelable(false);
+            mConnectionProgressDialog.show();
+
+            if (!mDrone.disconnect()) {
+                finish();
+            }
+        }
+    }
+
+    private void initIHM() {}
+
+    private final Drone.Listener mDroneListener = new Drone.Listener() {
+        @Override
+        public void onDroneConnectionChanged(ARCONTROLLER_DEVICE_STATE_ENUM state) {
+            switch (state)
+            {
+                case ARCONTROLLER_DEVICE_STATE_RUNNING:
+                    mConnectionProgressDialog.dismiss();
+                    break;
+
+                case ARCONTROLLER_DEVICE_STATE_STOPPED:
+                    // if the deviceController is stopped, go back to the previous activity
+                    mConnectionProgressDialog.dismiss();
+                    finish();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void onBatteryChargeChanged(int batteryPercentage) {
+            mBatteryLabel.setText(String.format("%d%%", batteryPercentage));
+        }
+
+        @Override
+        public void onPilotingStateChanged(ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM state) {
+            switch (state) {
+                case ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_LANDED:
+
+                    break;
+                case ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_FLYING:
+                case ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_HOVERING:
+
+                    break;
+                default:
+
+            }
+        }
+
+        @Override
+        public void onPictureTaken(ARCOMMANDS_ARDRONE3_MEDIARECORDEVENT_PICTUREEVENTCHANGED_ERROR_ENUM error) {
+            Log.i(TAG, "Picture has been taken");
+        }
+
+        @Override
+        public void configureDecoder(ARControllerCodec codec) {
+            mVideoView.configureDecoder(codec);
+        }
+
+        @Override
+        public void onFrameReceived(ARFrame frame) {
+            mVideoView.displayFrame(frame);
+        }
+
+        @Override
+        public void onMatchingMediasFound(int nbMedias) {
+            mDownloadProgressDialog.dismiss();
+
+            mNbMaxDownload = nbMedias;
+            mCurrentDownloadIndex = 1;
+
+            if (nbMedias > 0) {
+                mDownloadProgressDialog = new ProgressDialog(DroneActivity.this, R.style.AppCompatAlertDialogStyle);
+                mDownloadProgressDialog.setIndeterminate(false);
+                mDownloadProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                mDownloadProgressDialog.setMessage("Downloading medias");
+                mDownloadProgressDialog.setMax(mNbMaxDownload * 100);
+                mDownloadProgressDialog.setSecondaryProgress(mCurrentDownloadIndex * 100);
+                mDownloadProgressDialog.setProgress(0);
+                mDownloadProgressDialog.setCancelable(false);
+                mDownloadProgressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mDrone.cancelGetLastFlightMedias();
+                    }
+                });
+                mDownloadProgressDialog.show();
+            }
+        }
+
+        @Override
+        public void onDownloadProgressed(String mediaName, int progress) {
+            mDownloadProgressDialog.setProgress(((mCurrentDownloadIndex - 1) * 100) + progress);
+        }
+
+        @Override
+        public void onDownloadComplete(String mediaName) {
+            mCurrentDownloadIndex++;
+            mDownloadProgressDialog.setSecondaryProgress(mCurrentDownloadIndex * 100);
+
+            if (mCurrentDownloadIndex > mNbMaxDownload) {
+                mDownloadProgressDialog.dismiss();
+                mDownloadProgressDialog = null;
+            }
+        }
+    };
 }
