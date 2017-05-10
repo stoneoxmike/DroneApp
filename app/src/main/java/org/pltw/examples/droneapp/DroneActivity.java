b3389c2b1845509bfb6a66ace0e501c1abc3036f
stoneoxmike <stoneoxmike@gmail.com>
2017-05-10 12:40:11 -0400

    5/10/17

--- app/src/main/java/org/pltw/examples/droneapp/DroneActivity.java
+++ app/src/main/java/org/pltw/examples/droneapp/DroneActivity.java
@@ -3,11 +3,19 @@ package org.pltw.examples.droneapp;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.location.Location;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -25,33 +33,57 @@ import com.parrot.arsdk.ARSDK;
 import com.parrot.arsdk.arcommands.ARCOMMANDS_ARDRONE3_MEDIARECORDEVENT_PICTUREEVENTCHANGED_ERROR_ENUM;
 import com.parrot.arsdk.arcommands.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM;
 import com.parrot.arsdk.arcontroller.ARCONTROLLER_DEVICE_STATE_ENUM;
+import com.parrot.arsdk.arcontroller.ARCONTROLLER_DICTIONARY_KEY_ENUM;
+import com.parrot.arsdk.arcontroller.ARCONTROLLER_ERROR_ENUM;
+import com.parrot.arsdk.arcontroller.ARControllerArgumentDictionary;
 import com.parrot.arsdk.arcontroller.ARControllerCodec;
+import com.parrot.arsdk.arcontroller.ARControllerDictionary;
+import com.parrot.arsdk.arcontroller.ARControllerException;
+import com.parrot.arsdk.arcontroller.ARDeviceController;
+import com.parrot.arsdk.arcontroller.ARDeviceControllerListener;
+import com.parrot.arsdk.arcontroller.ARFeatureARDrone3;
+import com.parrot.arsdk.arcontroller.ARFeatureCommon;
 import com.parrot.arsdk.arcontroller.ARFrame;
+import com.parrot.arsdk.ardiscovery.ARDISCOVERY_PRODUCT_ENUM;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDevice;
 import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceService;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryException;
 import com.parrot.arsdk.ardiscovery.ARDiscoveryService;
+import com.parrot.arsdk.ardiscovery.receivers.ARDiscoveryServicesDevicesListUpdatedReceiver;
+import com.parrot.arsdk.ardiscovery.receivers.ARDiscoveryServicesDevicesListUpdatedReceiverDelegate;
+import com.parrot.arsdk.arsal.ARSALPrint;
 
+import java.util.List;
+
+import static com.google.android.gms.wearable.DataMap.TAG;
 import static com.parrot.arsdk.arcommands.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_HOVERING;
 import static com.parrot.arsdk.ardiscovery.ARDISCOVERY_PRODUCT_ENUM.ARDISCOVERY_PRODUCT_ARDRONE;
 
 
-public class DroneActivity extends FragmentActivity implements OnMapReadyCallback {
+public class DroneActivity extends FragmentActivity implements OnMapReadyCallback, ARDeviceControllerListener {
     private static final String TAG = "DroneActivity";
-    private Drone mDrone;
-
-    private ProgressDialog mConnectionProgressDialog;
-    private ProgressDialog mDownloadProgressDialog;
-
-    private DroneVideoView mVideoView;
-
-    private TextView mBatteryLabel;
-    //private Button mTakeOffLandBt;
-    //private Button mDownloadBt;
-
-    private int mNbMaxDownload;
-    private int mCurrentDownloadIndex;
     private GoogleMap mMap;
     private Double Lat;
     private Double Lng;
+    private BroadcastReceiver mArdiscoveryServicesDevicesListUpdatedReceiver;
+    private ARDiscoveryDeviceService mDeviceService;
+    private ARDeviceController deviceController;
+    private ARDiscoveryDevice device;
+    private ARDiscoveryService mArdiscoveryService;
+    private ServiceConnection mArdiscoveryServiceConnection;
+    private final ARDiscoveryServicesDevicesListUpdatedReceiverDelegate mDiscoveryDelegate =
+            new ARDiscoveryServicesDevicesListUpdatedReceiverDelegate() {
+
+                @Override
+                public void onServicesDevicesListUpdated() {
+                    if (mArdiscoveryService != null) {
+                        List<ARDiscoveryDeviceService> deviceList = mArdiscoveryService.getDeviceServicesArray();
+                        mDeviceService = deviceList.get(0);
+                        // Do what you want with the device list
+                    }
+                }
+            };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,17 +93,32 @@ public class DroneActivity extends FragmentActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        initIHM();
-
-        Intent intent = getIntent();
-
-        mDrone = new Drone(this, service);
-        mDrone.addListener(mDroneListener);
         Location currentLocation = new Location("Project Fi");
         Lat = currentLocation.getLatitude();
         Lng = currentLocation.getLongitude();
         StartDialogFragment startDialogFragment = new StartDialogFragment();
         startDialogFragment.show(getFragmentManager(), "start");
+        ARSDK.loadSDKLibs();
+        initDiscoveryService();
+        registerReceivers();
+        ARDiscoveryDevice discoveryDevice = createDiscoveryDevice(mDeviceService);
+        if (discoveryDevice != null) {
+            try
+            {
+                deviceController = new ARDeviceController(device);
+                discoveryDevice.dispose();
+            }
+            catch (ARControllerException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        // your class should implement ARDeviceControllerListener
+        deviceController.addListener (this);
+        ARCONTROLLER_ERROR_ENUM error = deviceController.start();
+
+
+        takeoff();
     }
 
 
@@ -94,139 +141,202 @@ public class DroneActivity extends FragmentActivity implements OnMapReadyCallbac
         mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocationMarker));
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
 
-        // show a loading view while the bebop drone is connecting
-        if ((mDrone != null) && !(ARCONTROLLER_DEVICE_STATE_ENUM.ARCONTROLLER_DEVICE_STATE_RUNNING.equals(mDrone.getConnectionState())))
+
+    private void initDiscoveryService()
+    {
+        // create the service connection
+        if (mArdiscoveryServiceConnection == null)
         {
-            mConnectionProgressDialog = new ProgressDialog(this, R.style.AppCompatAlertDialogStyle);
-            mConnectionProgressDialog.setIndeterminate(true);
-            mConnectionProgressDialog.setMessage("Connecting ...");
-            mConnectionProgressDialog.setCancelable(false);
-            mConnectionProgressDialog.show();
-
-            // if the connection to the Bebop fails, finish the activity
-            if (!mDrone.connect()) {
-                finish();
-            }
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (mDrone != null)
-        {
-            mConnectionProgressDialog = new ProgressDialog(this, R.style.AppCompatAlertDialogStyle);
-            mConnectionProgressDialog.setIndeterminate(true);
-            mConnectionProgressDialog.setMessage("Disconnecting ...");
-            mConnectionProgressDialog.setCancelable(false);
-            mConnectionProgressDialog.show();
-
-            if (!mDrone.disconnect()) {
-                finish();
-            }
-        }
-    }
-
-    private void initIHM() {}
-
-    private final Drone.Listener mDroneListener = new Drone.Listener() {
-        @Override
-        public void onDroneConnectionChanged(ARCONTROLLER_DEVICE_STATE_ENUM state) {
-            switch (state)
+            mArdiscoveryServiceConnection = new ServiceConnection()
             {
-                case ARCONTROLLER_DEVICE_STATE_RUNNING:
-                    mConnectionProgressDialog.dismiss();
-                    break;
+                @Override
+                public void onServiceConnected(ComponentName name, IBinder service)
+                {
+                    mArdiscoveryService = ((ARDiscoveryService.LocalBinder) service).getService();
 
-                case ARCONTROLLER_DEVICE_STATE_STOPPED:
-                    // if the deviceController is stopped, go back to the previous activity
-                    mConnectionProgressDialog.dismiss();
-                    finish();
-                    break;
+                    startDiscovery();
+                }
 
-                default:
-                    break;
+                @Override
+                public void onServiceDisconnected(ComponentName name)
+                {
+                    mArdiscoveryService = null;
+                }
+            };
+        }
+
+        if (mArdiscoveryService == null)
+        {
+            // if the discovery service doesn't exists, bind to it
+            Intent i = new Intent(getApplicationContext(), ARDiscoveryService.class);
+            getApplicationContext().bindService(i, mArdiscoveryServiceConnection, Context.BIND_AUTO_CREATE);
+        }
+        else
+        {
+            // if the discovery service already exists, start discovery
+            startDiscovery();
+        }
+    }
+
+    private void startDiscovery()
+    {
+        if (mArdiscoveryService != null)
+        {
+            mArdiscoveryService.start();
+        }
+    }
+    private void registerReceivers()
+    {
+        ARDiscoveryServicesDevicesListUpdatedReceiver receiver =
+                new ARDiscoveryServicesDevicesListUpdatedReceiver(mDiscoveryDelegate);
+        LocalBroadcastManager localBroadcastMgr = LocalBroadcastManager.getInstance(getApplicationContext());
+        localBroadcastMgr.registerReceiver(receiver,
+                new IntentFilter(ARDiscoveryService.kARDiscoveryServiceNotificationServicesDevicesListUpdated));
+    }
+
+
+    private ARDiscoveryDevice createDiscoveryDevice(@NonNull ARDiscoveryDeviceService service) {
+        ARDiscoveryDevice device = null;
+        try {
+            device = new ARDiscoveryDevice(getApplicationContext(), service);
+        } catch (ARDiscoveryException e) {
+            Log.e(TAG, "Exception", e);
+        }
+
+        return device;
+    }
+    private void unregisterReceivers()
+    {
+        LocalBroadcastManager localBroadcastMgr = LocalBroadcastManager.getInstance(getApplicationContext());
+
+        localBroadcastMgr.unregisterReceiver(mArdiscoveryServicesDevicesListUpdatedReceiver);
+    }
+
+    private void closeServices()
+    {
+        Log.d(TAG, "closeServices ...");
+
+        if (mArdiscoveryService != null)
+        {
+            new Thread(new Runnable() {
+                @Override
+                public void run()
+                {
+                    mArdiscoveryService.stop();
+
+                    getApplicationContext().unbindService(mArdiscoveryServiceConnection);
+                    mArdiscoveryService = null;
+                }
+            }).start();
+        }
+    }
+
+    @Override
+// called when the state of the device controller has changed
+    public void onStateChanged (ARDeviceController deviceController, ARCONTROLLER_DEVICE_STATE_ENUM newState, ARCONTROLLER_ERROR_ENUM error)
+    {
+        switch (newState)
+        {
+            case ARCONTROLLER_DEVICE_STATE_RUNNING:
+                break;
+            case ARCONTROLLER_DEVICE_STATE_STOPPED:
+                break;
+            case ARCONTROLLER_DEVICE_STATE_STARTING:
+                break;
+            case ARCONTROLLER_DEVICE_STATE_STOPPING:
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onExtensionStateChanged(ARDeviceController deviceController, ARCONTROLLER_DEVICE_STATE_ENUM newState, ARDISCOVERY_PRODUCT_ENUM product, String name, ARCONTROLLER_ERROR_ENUM error) {
+
+    }
+
+    @Override
+// called when a command has been received from the drone
+    public void onCommandReceived(ARDeviceController deviceController, ARCONTROLLER_DICTIONARY_KEY_ENUM commandKey, ARControllerDictionary elementDictionary)
+    {
+        if (elementDictionary != null)
+        {
+            // if the command received is a battery state changed
+            if (commandKey == ARCONTROLLER_DICTIONARY_KEY_ENUM.ARCONTROLLER_DICTIONARY_KEY_COMMON_COMMONSTATE_BATTERYSTATECHANGED)
+            {
+                ARControllerArgumentDictionary<Object> args = elementDictionary.get(ARControllerDictionary.ARCONTROLLER_DICTIONARY_SINGLE_KEY);
+
+                if (args != null)
+                {
+                    Integer batValue = (Integer) args.get(ARFeatureCommon.ARCONTROLLER_DICTIONARY_KEY_COMMON_COMMONSTATE_BATTERYSTATECHANGED_PERCENT);
+
+                    // do what you want with the battery level
+                }
             }
         }
-
-        @Override
-        public void onBatteryChargeChanged(int batteryPercentage) {
-            mBatteryLabel.setText(String.format("%d%%", batteryPercentage));
+        else
+        {
+            Log.e(TAG, "elementDictionary is null");
         }
+    }
 
-        @Override
-        public void onPilotingStateChanged(ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM state) {
-            switch (state) {
-                case ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_LANDED:
-
-                    break;
-                case ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_FLYING:
-                case ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_HOVERING:
-
-                    break;
-                default:
-
-            }
-        }
-
-        @Override
-        public void onPictureTaken(ARCOMMANDS_ARDRONE3_MEDIARECORDEVENT_PICTUREEVENTCHANGED_ERROR_ENUM error) {
-            Log.i(TAG, "Picture has been taken");
-        }
-
-        @Override
-        public void configureDecoder(ARControllerCodec codec) {
-            mVideoView.configureDecoder(codec);
-        }
-
-        @Override
-        public void onFrameReceived(ARFrame frame) {
-            mVideoView.displayFrame(frame);
-        }
-
-        @Override
-        public void onMatchingMediasFound(int nbMedias) {
-            mDownloadProgressDialog.dismiss();
-
-            mNbMaxDownload = nbMedias;
-            mCurrentDownloadIndex = 1;
-
-            if (nbMedias > 0) {
-                mDownloadProgressDialog = new ProgressDialog(DroneActivity.this, R.style.AppCompatAlertDialogStyle);
-                mDownloadProgressDialog.setIndeterminate(false);
-                mDownloadProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-                mDownloadProgressDialog.setMessage("Downloading medias");
-                mDownloadProgressDialog.setMax(mNbMaxDownload * 100);
-                mDownloadProgressDialog.setSecondaryProgress(mCurrentDownloadIndex * 100);
-                mDownloadProgressDialog.setProgress(0);
-                mDownloadProgressDialog.setCancelable(false);
-                mDownloadProgressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        mDrone.cancelGetLastFlightMedias();
+    private ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM getPilotingState()
+    {
+        ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM flyingState = ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.eARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_UNKNOWN_ENUM_VALUE;
+        if (deviceController != null)
+        {
+            try
+            {
+                ARControllerDictionary dict = deviceController.getCommandElements(ARCONTROLLER_DICTIONARY_KEY_ENUM.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED);
+                if (dict != null)
+                {
+                    ARControllerArgumentDictionary<Object> args = dict.get(ARControllerDictionary.ARCONTROLLER_DICTIONARY_SINGLE_KEY);
+                    if (args != null)
+                    {
+                        Integer flyingStateInt = (Integer) args.get(ARFeatureARDrone3.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE);
+                        flyingState = ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.getFromValue(flyingStateInt);
                     }
-                });
-                mDownloadProgressDialog.show();
+                }
+            }
+            catch (ARControllerException e)
+            {
+                e.printStackTrace();
+            }
+
+
+        }
+        return flyingState;
+    }
+
+    private void takeoff()
+    {
+        if (ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_LANDED.equals(getPilotingState()))
+        {
+            ARCONTROLLER_ERROR_ENUM error = deviceController.getFeatureARDrone3().sendPilotingTakeOff();
+
+            if (!error.equals(ARCONTROLLER_ERROR_ENUM.ARCONTROLLER_OK))
+            {
+                ARSALPrint.e(TAG, "Error while sending take off: " + error);
             }
         }
+    }
 
-        @Override
-        public void onDownloadProgressed(String mediaName, int progress) {
-            mDownloadProgressDialog.setProgress(((mCurrentDownloadIndex - 1) * 100) + progress);
-        }
+    private void land()
+    {
+        ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM flyingState = getPilotingState();
+        if (ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_HOVERING.equals(flyingState) ||
+                ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_HOVERING.equals(flyingState))
+        {
+            ARCONTROLLER_ERROR_ENUM error = deviceController.getFeatureARDrone3().sendPilotingLanding();
 
-        @Override
-        public void onDownloadComplete(String mediaName) {
-            mCurrentDownloadIndex++;
-            mDownloadProgressDialog.setSecondaryProgress(mCurrentDownloadIndex * 100);
-
-            if (mCurrentDownloadIndex > mNbMaxDownload) {
-                mDownloadProgressDialog.dismiss();
-                mDownloadProgressDialog = null;
+            if (!error.equals(ARCONTROLLER_ERROR_ENUM.ARCONTROLLER_OK))
+            {
+                ARSALPrint.e(TAG, "Error while sending take off: " + error);
             }
         }
-    };
+    }
+
 }
+
